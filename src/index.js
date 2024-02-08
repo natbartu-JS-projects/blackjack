200ed70fae103e494a076dbccf658df8092982b3
natacha <nat_bartu@hotmail.com>
2024-02-08 12:09:36 +1000

    add boolean conditional

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 let cardOne = document.getElementById("card1");
 let cardTwo = document.getElementById("card2");
 
-let firstCard = 10;
+let firstCard = 12;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
+let isAlive = true;
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
@@ -13,4 +14,7 @@ if (sum <= 20) {
   hasBlackJack = true;
 } else {
   console.log("You're out of the game! 😭");
+  isAlive = false;
 }
+
+console.log(isAlive);
