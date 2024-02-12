94df54ae98f1b2e16fec47d045163ea1f50253eb
natacha <nat_bartu@hotmail.com>
2024-02-12 12:04:58 +1000

    change game array and values to be empty once game start and change it functionality into startGame function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = " ";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
@@ -11,11 +9,24 @@ let cardEl = document.getElementById("card-el");
 let gameOverMessage = document.getElementById("game-over-message");
 
 function getRandomCard() {
-  let cardNumber = Math.floor(Math.random() * 13) + 1;
-  return cardNumber;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+
   renderGame();
 }
 
